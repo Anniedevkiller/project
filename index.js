74b839dc4e7b9705dc6492d4b0f3996e875d7ec0
anniedevkiller <erisannieannie@gmail.com>
2024-07-02 22:33:52 +0100

    stage 1 task is completed

---
+++ index.js
@@ -0,0 +1,15 @@
+function updateTimestamp(){
+    const timestampElement= document.getElementById('time');
+    const dayElement= document.getElementById('day');
+    const currentDate= new Date();
+    const daysOfWeek= ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dayOfWeek= daysOfWeek[currentDate.getUTCDay()];
+    const formattedTime= currentDate.toLocaleTimeString('en-US',{timeZone: 'UTC', hour12:true, hour:'numeric', minute:'numeric', second:'numeric'
+    });
+
+    timestampElement.textContent= formattedTime;
+    dayElement.textContent= dayOfWeek;
+}
+//Update the timestamp initially and every second//
+updateTimestamp();
+setInterval(updateTimestamp,1000);
